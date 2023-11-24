aaccefbb285cda50d3881d3a9f60b4f170dadabd
ziad ahmed mohamed <92855581+StrykerEG@users.noreply.github.com>
2023-11-24 15:51:38 +0200

    add another basic setup for project

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ import pandas as pd
 symbol_table = {}
 
 
+def parsing_location_counter(location_counter):
+    if(type(location_counter == "str")):
+        location_counter = int(location_counter)
+    elif(type(location_counter == "int")):
+        location_counter = hex(location_counter)
+    return location_counter
+         
+
+
 if __name__ == "__main__":
     table = pd.read_csv("table.csv")
     table["Address"] = "" #adding new column
@@ -21,14 +30,20 @@ if __name__ == "__main__":
 
             table.at[index,"Address"] = starting_address
             symbol_table["label"] = location_counter
-            print(symbol_table.keys())
         elif(instructions == "RESB" or instructions == "RESW"):
             pass
+        elif(instructions == "Byte"):
+            if (reference.startswith("C'")):
+                pass
+            elif(reference.startswith("X'")):
+                pass
+
         else:
-            location_counter = int(location_counter)
+            location_counter = parsing_location_counter(location_counter)
             location_counter +=3
+            print(type(location_counter))
             table.at[index,"Address"] = location_counter 
-            location_counter= str(location_counter)
+    
     
     print(table)
 
